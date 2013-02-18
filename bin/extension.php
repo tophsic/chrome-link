d632695c667aceb213ab87eb85fa8992627e0ca4
Christophe Sicard <sicard.christophe@gmail.com>
2013-02-18 11:10:19 +0100

    extension dev

---
+++ bin/extension.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use MyLink\Manifest;
+
+$console = new Application();
+
+$console
+	->register('name')
+	->setDefinition(array(
+			new InputArgument('name', InputArgument::REQUIRED, 'Name'),
+	))
+	->setDescription('Change name of the extension')
+	->setCode(function (InputInterface $input, OutputInterface $output) {
+		$manifest = new Manifest(__DIR__ . '/../manifest.json');
+		$manifest->config(array(
+			'name' => $input->getArgument('name')
+		));
+		$output->writeln('Done');
+	})
+	;
+	
+$console
+	->register('description')
+	->setDefinition(array(
+			new InputArgument('description', InputArgument::REQUIRED, 'Description'),
+	))
+	->setDescription('Change description of the extension')
+	->setCode(function (InputInterface $input, OutputInterface $output) {
+		$manifest = new Manifest(__DIR__ . '/../manifest.json');
+		$manifest->config(array(
+			'description' => $input->getArgument('description')
+		));
+		$output->writeln('Done');
+	})
+	;
+	
+$console
+	->register('url')
+	->setDefinition(array(
+			new InputArgument('url', InputArgument::REQUIRED, 'Url'),
+	))
+	->setDescription('Change url')
+	->setCode(function (InputInterface $input, OutputInterface $output) {
+		$manifest = new Manifest(__DIR__ . '/../manifest.json');
+		$manifest->config(array(
+			'url' => $input->getArgument('url')
+		));
+		$output->writeln('Done');
+	})
+	;
+	
+$console
+	->register('config')
+	->setDefinition(array(
+			new InputArgument('name', InputArgument::OPTIONAL, 'Name'),
+			new InputArgument('description', InputArgument::OPTIONAL, 'Description'),
+			new InputArgument('url', InputArgument::OPTIONAL, 'Url'),
+	))
+	->setDescription('Change name of the extension')
+	->setCode(function (InputInterface $input, OutputInterface $output) {
+		$manifest = new Manifest(__DIR__ . '/../manifest.json');
+		$manifest->config(array(
+			'name' => $input->getArgument('name'),
+			'description' => $input->getArgument('description'),
+			'url' => $input->getArgument('url')
+		));
+		$output->writeln('Done');
+	})
+	;
+
+$console->run();
---
+++ src/MyLink/Manifest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace MyLink;
+
+use \LogicException;
+
+class Manifest
+{
+	private $filename,
+			$json;
+	
+	public function __construct($filename)
+	{
+		if( !is_file($filename) )
+		{
+			throw new LogicException('File name does not exist.'); 
+		}
+		
+		$this->filename = $filename;
+		
+		$this->json = json_decode(file_get_contents($this->filename));
+		
+		if( is_null($this->json) )
+		{
+			throw new LogicException('Manifest is not well formated.');
+		}
+	}
+	
+	public function config(array $config)
+	{
+		foreach( $config as $key => $value )
+		{
+			$methodName = 'set' . ucfirst(strtolower($key));
+			
+			if( !method_exists($this, $methodName))
+			{
+				throw new LogicException(
+					sprintf(
+						'Config %s is not implemented',
+						$key
+					)
+				);
+			}
+			
+			$this->$methodName($value);
+		}
+		
+		$this->save();
+		
+		return $this;
+	}
+	
+	public function save()
+	{
+		if( is_null($this->filename)
+			&& is_null($this->json) )
+		{
+			return $this;
+		}
+		
+		file_put_contents($this->filename, json_encode($this->json, JSON_PRETTY_PRINT));
+		
+		return $this;
+	}
+	
+	protected function setName($name)
+	{
+		$this->json->name = (string) $name;
+	}
+
+	protected function setDescription($description)
+	{
+		$this->json->description = (string) $description;
+	}
+
+	protected function setUrl($url)
+	{
+		$this->json->app->launch->web_url = (string) $url;
+		$this->json->app->web_content->origin = (string) $url;
+	}
+}
